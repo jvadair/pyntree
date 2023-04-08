c25e6f493477c599e66878f15b9866071d7f1a7d
jvadair <dev@jvadair.com>
2023-04-08 12:54:44 -0400

    Add checks for setting/changing the password after initializing

--- tests/tests.py
+++ tests/tests.py
@@ -194,6 +194,21 @@ class FileSaving(unittest.TestCase):
         db.save('tests/testing_save_NiN.pyn')
         os.remove('tests/testing_save_NiN.pyn')
 
+    def test_save_after_password_set(self):
+        db = Node({'a': 'b'})
+        db.file.password = 'testing'
+        db.save('tests/testing_encrypted_setpwd.pyn')
+        self.assertEqual(Node('tests/testing_encrypted_setpwd.pyn', password='testing')(), {'a': 'b'})
+        os.remove('tests/testing_encrypted_setpwd.pyn')
+
+    def test_save_after_password_change(self):
+        db = Node('tests/testing_encrypted_changepwd.pyn', password='testing1')
+        db.a = 'b'
+        db.file.password = 'testing2'
+        db.save('tests/testing_encrypted_changepwd.pyn')
+        self.assertEqual(Node('tests/testing_encrypted_changepwd.pyn', password='testing2')(), {'a': 'b'})
+        os.remove('tests/testing_encrypted_changepwd.pyn')
+
 
 class DeletionTests(unittest.TestCase):
     def test_layer_0(self):
